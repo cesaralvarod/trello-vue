141bd486b0036ed756ee9a20ef6b44b939196423
cesaralvarod <cesaralvarod@gmail.com>
2022-04-19 00:42:06 -0500

    update 19042022

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <app-header></app-header>
+  <app-header>
+  </app-header>
   <router-view class="max-w-[1600px] mx-auto py-5 px-4 mb-20"></router-view>
   <app-footer></app-footer>
 </template>
@@ -11,6 +12,7 @@ import AppFooter from "./components/AppFooter.vue";
 export default {
   name: "App",
   components: { AppHeader, AppFooter },
+
 };
 </script>
 
